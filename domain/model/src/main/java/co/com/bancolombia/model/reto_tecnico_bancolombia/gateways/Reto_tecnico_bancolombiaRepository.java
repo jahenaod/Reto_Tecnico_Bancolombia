c58a7852c0eabd8d46f4129eebecff8db8da981f
JUAANHEN <juaanhen@bancolombia.com.co>
2023-05-16 22:17:52 -0500

    first commit

---
+++ domain/model/src/main/java/co/com/bancolombia/model/reto_tecnico_bancolombia/gateways/Reto_tecnico_bancolombiaRepository.java
@@ -0,0 +1,4 @@
+package co.com.bancolombia.model.reto_tecnico_bancolombia.gateways;
+
+public interface Reto_tecnico_bancolombiaRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/UseCase.java
@@ -0,0 +1,6 @@
+package co.com.bancolombia.usecase.;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class UseCase {
+}
